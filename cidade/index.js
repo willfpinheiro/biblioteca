35210d12f3eaa81838441eab9a0a33b44a847acf
willfpinheiro <42159903+willfpinheiro@users.noreply.github.com>
2020-11-15 19:43:56 -0300

    Criado map para listar cidades e estados

--- cidade/index.js
+++ cidade/index.js
@@ -3,12 +3,20 @@
     axios
   .get(`https://cadastro-pessoas-cassio.herokuapp.com/api/v1/cidades/${siglaCIdade}`)
   .then(function (response) {
-    const cidades = JSON.stringify(response.data)
+    const cidades = response.data
     console.log(cidades);
+    function cidadesListadas() {
+      listaDeCidade = ''
+      cidades.map(cidade => {
+        listaDeCidade = listaDeCidade+`<li key=${cidade.id}> ${cidade.nome} </li>`
+      }
+      )
+      return listaDeCidade
+    }
     document.querySelector('.text').innerHTML = `
     <h1>Cidades</h1>
     <ul>
-      <li> ${cidades} </li>
+      ${cidadesListadas()}
     </ul>
 `
   })
--- estado/index.js
+++ estado/index.js
@@ -1,14 +1,43 @@
-  function getCidades() {
-    const siglaCIdade = document.querySelector('#siglaCidade').value.toUpperCase();
+  function getEstado() {
+    const siglaEstado = document.querySelector('#siglaEstado').value.toUpperCase()
     axios
-  .get(`https://cadastro-pessoas-cassio.herokuapp.com/api/v1/cidades/${siglaCIdade}`)
+  .get(`https://cadastro-pessoas-cassio.herokuapp.com/api/v1/estados/${siglaEstado}`)
   .then(function (response) {
-    const cidades = JSON.stringify(response.data)
-    console.log(cidades);
+    const estados = response.data.nome
+    console.log(estados);
     document.querySelector('.text').innerHTML = `
-    <h1>Cidades</h1>
+    <h1>Estado</h1>
     <ul>
-      <li> ${cidades} </li>
+      ${estados}
+    </ul>
+`
+  })
+  .catch(function (error) {
+    const { data, status } = error.response;
+    console.warn(`Status ${status}`);
+    console.warn(`Dados ${JSON.stringify(data.errors)}`);
+});
+document.querySelector('#siglaEstado').value = ''
+  }
+
+  function getTodosEstados() {
+    axios
+  .get(`https://cadastro-pessoas-cassio.herokuapp.com/api/v1/estados`)
+  .then(function (response) {
+    const estados = response.data
+    console.log(estados);
+    function estadoListadas() {
+      listaEstado = ''
+      estados.map(cidade => {
+        listaEstado = listaEstado+`<li key=${cidade.id}> ${cidade.nome} </li>`
+      }
+      )
+      return listaEstado
+    }
+    document.querySelector('.text').innerHTML = `
+    <h1>Estado</h1>
+    <ul>
+      ${estadoListadas()}
     </ul>
 `
   })
