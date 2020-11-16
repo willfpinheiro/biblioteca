05940d046ba02fd633514be952d7b78effe3a5e9
willfpinheiro <42159903+willfpinheiro@users.noreply.github.com>
2020-11-16 12:13:41 -0300

    Criado rotas para adicionar contato pessoa fisica

---
+++ contato/index.js
@@ -0,0 +1,58 @@
+  function getContatos() {
+    const idPessoa = document.querySelector('#pessoaFisica').value
+    axios
+  .get(`https://cadastro-pessoas-cassio.herokuapp.com/api/v1/contatos/${idPessoa}`)
+  .then(function (response) {
+    const contatosPessoa = JSON.stringify(response.data);
+    console.log(contatosPessoa);
+    document.querySelector('.text').innerHTML = `
+    <h1>Estado</h1>
+    <ul>
+      ${contatosPessoa}
+    </ul>
+`
+  })
+  .catch(function (error) {
+    const { data, status } = error.response;
+    console.warn(`Status ${status}`);
+    console.warn(`Dados ${JSON.stringify(data.errors)}`);
+});
+  }
+
+  function deleteContatos() {
+    const idPessoa = document.querySelector('#pessoaFisica').value
+    axios
+  .delete(`https://cadastro-pessoas-cassio.herokuapp.com/api/v1/contatos/${idPessoa}`)
+  .catch(function (error) {
+    const { data, status } = error.response;
+    console.warn(`Status ${status}`);
+    console.warn(`Dados ${JSON.stringify(data.errors)}`);
+});
+  }
+
+  function getPessoasPorNome() {
+    axios
+  .get(`https://cadastro-pessoas-cassio.herokuapp.com/api/v1/pessoaFisicas`)
+  .then(function (response) {
+    const pessoasFisicas = response.data
+    console.log(pessoasFisicas);
+    function pessoasListadas() {
+      listaPessoa = ''
+      pessoasFisicas.map(pessoa => {
+        listaPessoa = listaPessoa+`<option value=${pessoa.id}> ${pessoa.nome} </option>`
+      }
+      )
+      return listaPessoa
+    }
+    document.querySelector('#pessoaFisica').innerHTML = `
+      ${pessoasListadas()}
+`
+  })
+  .catch(function (error) {
+    const { data, status } = error.response;
+    console.warn(`Status ${status}`);
+    console.warn(`Dados ${JSON.stringify(data.errors)}`);
+});
+  }
+
+  getPessoasPorNome();
